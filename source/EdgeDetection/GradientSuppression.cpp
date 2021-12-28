4b11a7e66982be8d9c4a4bed8d23244534c1d9c0
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2021-12-28 18:38:12 +0100

    Fix suppression at the borders

--- source/EdgeDetection/GradientSuppression.cpp
+++ source/EdgeDetection/GradientSuppression.cpp
@@ -8,9 +8,9 @@ namespace EdgeDetection
 		float width = gradientIntensity.getWidth();
 		float height = gradientIntensity.getHeight();
 
-		for (int x = 1; x < width - 1; x++)
+		for (int x = 0; x < width; x++)
 		{
-			for (int y = 1; y < height - 1; y++)
+			for (int y = 0; y < height; y++)
 			{
 				Vector2Int currPos = Vector2Int(x, y);
 
@@ -19,8 +19,8 @@ namespace EdgeDetection
 				Vector2Int neighbour2Pos = currPos + ((-1)*direction);
 				
 				Types::Byte currVal = gradientIntensity.at(currPos);
-				Types::Byte neighbour1Val = gradientIntensity.at(neighbour1Pos);
-				Types::Byte neighbour2Val = gradientIntensity.at(neighbour2Pos);
+				Types::Byte neighbour1Val = gradientIntensity.atSafe(neighbour1Pos, 0);
+				Types::Byte neighbour2Val = gradientIntensity.atSafe(neighbour2Pos, 0);
 
 				if (neighbour1Val > currVal || neighbour2Val > currVal)
 				{ // The current pixel is not a maximum
